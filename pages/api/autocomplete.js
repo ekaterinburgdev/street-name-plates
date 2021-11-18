5515db0d62aacac46785be57199f0b29fa0a57fd
Eugene <emr201000@gmail.com>
2021-11-18 14:12:48 +0500

    #10 GET_autocomplete?{street : str}&{maximumDelivery : int}

---
+++ pages/api/autocomplete.js
@@ -0,0 +1,31 @@
+var itemsjs = require('itemsjs')
+var data = require('../../data/streets.json');
+var streetItems = itemsjs(data, {
+    sortings: {
+        street_asc: {
+            field: 'street',
+            order: 'asc'
+        },
+        type_asc: {
+            field: 'type',
+            order: 'asc'
+        }
+    },
+    searchableFields: ['street', 'type']
+})
+
+
+export default function handler(req, res) {
+    const beginning = req.query.street
+    const maximumDelivery = req.query.maximumDelivery
+    var answer = streetItems.search({
+        sort: 'street_asc',
+        per_page: maximumDelivery,
+        filter: function(item) {
+            return item.street.slice(0, beginning.length).toLowerCase() === beginning.toLowerCase() ;
+          }
+    })
+    res.status(200).json({
+        streets: answer.data.items
+    })
+  }
