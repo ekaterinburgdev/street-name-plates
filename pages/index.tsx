09b8916ab0fc43d537e9b92837e790ec97757aa8
lev <levtrifonov@mail.ru>
2022-01-10 13:43:29 +0500

    пересоздал ветку

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,42 +16,43 @@ const Home = () => {
     const valueButtonSendOrderContext = {buttonSendOrderContext, setButtonSendOrderContext};
 
     return (
-        <ButtonSendOrderContext.Provider value={valueButtonSendOrderContext}>
-            <ChangeColorContext.Provider value={valueColorContext}>
-                <div style={{margin: '0px', padding: '0px'}}>
-                    <div style={{backgroundImage: `url("${colorContext.frontImage}")`}} className={Style.front}>
-                        <div className={Style.front_wrapper}>
-                            <h1 className={Style.h1_wrapper}>Заказ адресной<br/>таблички</h1>
-                            <p className={Style.p_wrapper}>Введите название улицы и номер дома</p>
-                            <StreetPlate/>
+        <>
+            <div className={Style.header}>
+            </div>
+            <div className={Style.description_wrapper}>
+                <h1 className={Style.description_heading}>Адресные таблички</h1>
+                <p className={Style.description_text}>
+                    В Екатеринбурге несколько тысяч зданий, на каждом
+                    из них висит как минимум одна адресная табличка.
+                    Мы смотрим на них каждый день и с их помощью
+                    ориентируемся в улицах и номерах домов. Это одна
+                    из самых часто встречающихся деталей городской
+                    навигации. Таблички могли бы выполнять не только
+                    функциональную роль, но и воспитывать вкус
+                    и насмотренность у жителей и гостей города.
+                </p>
+            </div>
+            <ButtonSendOrderContext.Provider value={valueButtonSendOrderContext}>
+                <ChangeColorContext.Provider value={valueColorContext}>
+                    <div style={{margin: '0px', padding: '0px'}}>
+                        <div style={{backgroundImage: `url("${colorContext.frontImage}")`}} className={Style.front}>
+                            <div className={Style.front_wrapper}>
+                                <h1 className={Style.h1_wrapper}>Заказ адресной<br/>таблички</h1>
+                                <p className={Style.p_wrapper}>Введите название улицы и номер дома</p>
+                                <StreetPlate/>
+                            </div>
+                        </div>
+                        <div className={Style.inputs}>
+                            <div className={Style.change_color_container}>
+                                <p className={Style.inputs_p_wrapper}>Фасад</p>
+                                <ChangeColor/>
+                            </div>
+                            <OrderForm/>
                         </div>
                     </div>
-                    <div className={Style.inputs}>
-                        <div className={Style.change_color_container}>
-                            <p className={Style.inputs_p_wrapper}>Фасад</p>
-                            <ChangeColor/>
-                        </div>
-                        <OrderForm/>
-                    </div>
-                </div>
-                {/*<div style={{backgroundImage: `url("${colorContext.frontImage}")`}}>*/}
-                {/*    <div className={Style.container_start}>*/}
-                {/*        <div>*/}
-                {/*            <h1>Заказ адресной таблички</h1>*/}
-                {/*            <p>Введите название улицы и номер дома</p>*/}
-                {/*        </div>*/}
-                {/*        <ChangeColor className={Style.change_color}/>*/}
-                {/*    </div>*/}
-                {/*    <div className={Style.container_start}>*/}
-                {/*        <div>*/}
-                {/*            <StreerPlate/>*/}
-                {/*            <Inputs/>*/}
-                {/*        </div>*/}
-                {/*        <Window/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-            </ChangeColorContext.Provider>
-        </ButtonSendOrderContext.Provider>
+                </ChangeColorContext.Provider>
+            </ButtonSendOrderContext.Provider>
+        </>
     )
 }
 
