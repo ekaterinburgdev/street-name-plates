9e6ebb32087732f3fe7dffc753427d1727990916
Eugene <emr201000@gmail.com>
2022-01-24 22:16:53 +0500

    fixed hist validation

--- pages/api/request.js
+++ pages/api/request.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
     return;
   }
 
-  const validation_info = validate(body["type"], body["street_name"], body["number"])
+  const validation_info = validate(body["type"], body["street_name"], body["number"].toString())
   if (!validation_info["is_valid"]) {
     return res.status(400).json({ });
   }
